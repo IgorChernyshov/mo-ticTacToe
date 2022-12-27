1d99e1e6a1e37ba24305b2f93daab2960e14de0e
Igor Chernyshov <ichernyshov@bk.ru>
2022-12-27 18:58:28 +0300

    Initiated Project

---
+++ mo-ticTacToe/mo_ticTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mo_ticTacToeApp.swift
+//  mo-ticTacToe
+//
+//  Created by Игорь Чернышов on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct mo_ticTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
